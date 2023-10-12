774464634419afd9b119419114e2248e6f87c6ca
Mechanel <nyutanislavsky@qq.com>
2023-10-12 16:14:03 +0800

    fix

--- 1v2brawl.lua
+++ 1v2brawl.lua
@@ -63,6 +63,7 @@ local brawl_getLogic = function()
 
     for k, p in ipairs(players) do
       local avatar = p._splayer:getAvatar()
+      if avatar == "anjiang" then avatar = table.random{ "blank_shibing", "blank_nvshibing" } end
       local avatar_general = Fk.generals[avatar] or Fk.generals["mouxusheng"]
       room:setPlayerGeneral(p, avatar_general.name, true)
       room:broadcastProperty(p, "general")
@@ -86,7 +87,9 @@ local brawl_getLogic = function()
 
     for _, p in ipairs(players) do
       local choice = p.reply_ready and json.decode(p.client_reply) or p.default_reply
-      room:setPlayerMark(p, "@brawl_skills", choice)
+      room:setPlayerMark(p, "_brawl_skills", choice)
+      choice = table.map(choice, function(s) return Fk:translate(s) end)
+      room:setPlayerMark(p, "@brawl_skills", "<font color='burlywood'>" .. table.concat(choice, " ") .. "</font>")
       p.default_reply = ""
     end
 
@@ -134,10 +137,11 @@ local brawl_getLogic = function()
   function brawl_logic:attachSkillToPlayers()
     local room = self.room
     for _, p in ipairs(room.alive_players) do
-      local skills = table.concat(p:getMark("@brawl_skills"), "|")
+      local skills = table.concat(p:getMark("_brawl_skills"), "|")
       if p.role == "lord" then
         skills = skills .."|m_feiyang|m_bahu"
       end
+      skills = skills .. "|cheat"
       room:handleAddLoseSkills(p, skills, nil, false)
     end
   end
